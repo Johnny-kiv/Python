b9eddb0a3b7536fa8d9b2376fa65b1c5598ff958
Иван <johnny.kiv@yandex.ru>
2021-01-29 22:53:56 +0300

    Create intger29.py

---
+++ intger29.py
@@ -0,0 +1,5 @@
+a=int(input('Enter the side of the rectangle x: '))
+b=int(input('Enter the side of the rectangle y: '))
+c=int(input('Enter the side of the square : '))
+d=int(a/c)+int(b/c)
+print('Can accommodate:',d);
