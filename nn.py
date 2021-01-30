322e5b645bbb6eb669ba5bcee0f399930ffc3acd
Иван <johnny.kiv@yandex.ru>
2021-01-30 20:36:12 +0300

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,15 +1,18 @@
-class Lampa(object):
-    def _iit_(self):
-        print('\a ');
-        print('Приветсвую хозяин я готов исполнить твои три желания')
-    def _t_(self):
-        a=10000
-        while a<0:
-            a=a-1
-            print('\a ');
-        print('Ку-ку-у!?')
-Djin = Lampa()
-Djin._iit_()
-
-
-Djin._t_()
+import random
+a=1
+b=0
+i=0
+while a:
+    i+i+1
+    rand=random.randint(1,10)
+    a=int(input('Число загадоно попробуй отгадать '))
+    if a==rand:
+        b=b+1
+        print('Правильно! Было число',a)
+    else:
+        print('Неправильно')
+print('До всречи в следуйший раз')
+if  b>=10:
+    print('Поздравляю, у вас рекорд.',b,'очков')
+else:
+    print('У вас было',b,'очков')
