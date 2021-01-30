1ba4d61a09d632a952676145de5ce19dcaaef350
Иван <johnny.kiv@yandex.ru>
2021-01-30 18:58:16 +0300

    Update nn.py

---
+++ nn.py
@@ -0,0 +1,15 @@
+class Lampa(object):
+    def _iit_(self):
+        print('\a ');
+        print('Приветсвую хозяин я готов исполнить твои три желания')
+    def _t_(self):
+        a=10000
+        while a<0:
+            a=a-1
+            print('\a ');
+        print('Ку-ку-у!?')
+Djin = Lampa()
+Djin._iit_()
+
+
+Djin._t_()
