fb0032119f006a6bdca99cf5907a3e873f6482a3
Иван <johnny.kiv@yandex.ru>
2021-01-30 18:02:12 +0300

    m

---
+++ if1.py
@@ -0,0 +1,4 @@
+a=int(input('Enter the number: '))
+if (a>0):
+   a=a+1
+print(' я ',a);
---
+++ integer30.py
@@ -0,0 +1,3 @@
+a=int(input('Enter the year number ')) 
+b=int(a/100)+1
+print("This is the",str(b)+'th century.')
