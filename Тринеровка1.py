5f121a324f29861726ce39fdf423471b2bf184cf
Иван <johnny.kiv@yandex.ru>
2021-01-31 22:05:54 +0300

    Update Тринеровка1.py

--- Тринеровка1.py
+++ Тринеровка1.py
@@ -1,4 +1,4 @@
-
+#Это тренировка
 
 from tkinter import*
 root=Tk()
@@ -6,11 +6,11 @@ m=Menu(root)
 root.config(menu=m)
 fm=Menu(m)
 def h():
-    
+    print('Hello, world')
 def mat():
    but1=Button(root)
    but1=["text"]="канкулятор"
-   
+   but.bing("Button-1",h)
    but1.pack()
 m.add_cascade(label='Книги',menu=fm)
 fm.add_command(label='Математика',command=mat)
