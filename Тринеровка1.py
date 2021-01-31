cdc4b77756237fc958f10f0de417bd85d90b8f31
Иван <johnny.kiv@yandex.ru>
2021-01-31 22:28:36 +0300

    Update Тринеровка1.py

--- Тринеровка1.py
+++ Тринеровка1.py
@@ -5,14 +5,19 @@ root=Tk()
 m=Menu(root)
 root.config(menu=m)
 fm=Menu(m)
-def h():
+def h(event):
     print('Hello, world')
 def mat():
-   but1=Button(root)
-   but1=["text"]="канкулятор"
-   but.bing("Button-1",h)
-   but1.pack()
-m.add_cascade(label='Книги',menu=fm)
+    but1=Button(root)
+    but1["text"]="канкулятор"
+    but1.bind("<Button-1>",h )
+    but1.pack()
+    but1=Button(root)
+    but1["text"]="1 класс"
+    but1.bind("<Button-1>",h )
+    but1.pack()
+m.add_cascade(label='Учебники',menu=fm)
 fm.add_command(label='Математика',command=mat)
+fm.add_command(label='Математика',command=russ)
 
 root.mainloop()
