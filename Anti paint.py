90ad01fcd37ce42b02f33e1ca652964244fc2eaf
Иван <johnny.kiv@yandex.ru>
2021-02-07 23:54:37 +0300

    hgjmj

--- Anti paint.py
+++ Anti paint.py
@@ -1,7 +1,7 @@
 from tkinter import*
 class Paint(Frame):
     def _init_(self,parent):
-        Frame._init_(self):
+        Frame._init_(self)
         self.parent=parent
         self.brush_size=10
         self.brush_color="white"
@@ -18,14 +18,14 @@ class Paint(Frame):
         self.pack(fill=BOTH,expand=1)
         self.columconfigure(6,weight=1)
         self.columconfigure(2,weight=1)
-        self.canv=Canvas(sefl.bg="black")
+        self.canv=Canvas(self.bg=="black")
         self.canv.grid(row=2,column=0,columnspan=7,padx=5,pady=5,sticky=E+W+S+N)
         self.canv.bind("<B1-Motion>",self.draw)
-        color_lab=Label(self,,text="Цвет")
+        color_lab=Label(self,text="Цвет")
         color_lab.grid(row=0,column=0,padx=6)
         red_btn=Button(self,text="Красный",width=10,command=lambda:self.set_color("red"))
         red_btn.grid(row=0,column=1,sticky=W)
-        color_lab=Label(self,,text="Размер кисти")
+        color_lab=Label(self,text="Размер кисти")
         color_lab.grid(row=0,column=0,padx=6)
         red_btn=Button(self,text="2х",width=10,command=lambda:self.set_brush_size(2))
         red_btn.grid(row=1,column=1,sticky=W)
@@ -34,5 +34,6 @@ class Paint(Frame):
         root.geometry("800x600+300+300")
         app = Paint(root)
         root.mainloop()
-    if _name_=="_main_":
+    name="_main_"
+    if name=="_main_":
         main()
--- bown/bown.py
+++ bown/bown.py
@@ -2,9 +2,9 @@ from tkinter import*
 import random
 win=Tk()
 c=Canvas(win,width=800,height=650,bg="grey",cursor="pencil")
-x=random.randint(1,790)
-a=1
-rect=c.create_oval(x,100,x+50,150,fill="red")
-c.move(rect,100,150)
 c.pack()
+
+x = random.randint(1, 790)
+z=c.create_oval(x, x, x + 100, x + 100, fill="red")
+
 win.mainloop()
