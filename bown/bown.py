dcd7879875fe651730856b3de8c8bc8e389fcc63
Иван <johnny.kiv@yandex.ru>
2021-02-13 20:25:21 +0300

    Update bown.py

--- bown/bown.py
+++ bown/bown.py
@@ -4,24 +4,25 @@ import time
 win=Tk()
 win.title("Bown")
 c=Canvas(win,width=1000,height=610,bg="grey",cursor="pencil")
-c.create_text(500,20,text="Правила игры очень просты: просто ловиш шарик.Если сочетания клавиш ctr-v, то вправоо,а лево ctr-c,а если нажмёш пробел то шарик будет падать.",fill="green")
+c.create_text(500,20,text="Правила игры очень просты: просто ловиш шарик.",fill="green")
 r=c.create_polygon(500,600,[550,550,],[450,550],fill="green")
-r2=c.create_oval(100,100,150,150,fill="blue")
 def l(event):
     c.move(r,-10,0)
 def p(event):
     c.move(r,10,0) 
-def lb(event):
-    c.move(r2,0,10)
 class Bown(object):
     def _init_(self):
-        win.bind('<Control-c>',l)
-        win.bind('<Control-v>',p)
+        win.bind('<Left>',l)
+        win.bind('<Right>',p)
 kor=Bown()
 kor._init_()   
 class V(object):
     def _init_(self):
-        win.bind('<space>',lb)
+        a=random.randint(1,990)
+        r2=c.create_oval(a,50,a+50,100,fill="blue")
+        time.sleep(1)
+        c.coords(r2,a,602,a+50,652)
+        r1=1
 k=V()
 k._init_()   
 c.pack()
