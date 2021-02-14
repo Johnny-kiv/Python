af9fd4ffdc9e51593390575228f5e5911f8ee90b
Иван <johnny.kiv@yandex.ru>
2021-02-14 22:26:36 +0300

    bbh

--- Игра/game.py
+++ Игра/game.py
@@ -1,3 +1,14 @@
+import random
 txt=open("input.txt","r")
 f = txt.read()
-print(int(f)+5)
+txt.close()
+r=1
+while r:
+    rand=random.randint(1,9)
+    c=int(f)+rand
+    if c<10:
+        b=c-int(f)
+        break
+txt2=open("outinput.txt","w")
+txt2.write(str(f)+str(c)+str(b))
+txt2.close()
