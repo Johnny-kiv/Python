9958ade777d8c6360bb005aec65f13f987c77c81
Иван <johnny.kiv@yandex.ru>
2021-02-14 21:44:30 +0300

    m

--- bascetball.py
+++ bascetball.py
@@ -1,18 +1,18 @@
 txt = open("hh.txt","r")
 r1=0
 r2=0
-f = txt.readline(1).split()
-r1=int(f[1])+r1
-r2=int(f[2])+r2
-f2 = txt.readline(2).split()
-r1=int(f2[1])+r1
-r2=int(f2[2])+r2
-f3= txt.readline(3).split()
-r1=int(f3[1])+r1
-r2=int(f3[2])+r2
-f4 = txt.readline(4).split()
-r1=int(f4[1])+r1
-r2=int(f4[2])+r2
+f = txt.readline().split()
+r1=int(f[0])+r1
+r2=int(f[1])+r2
+f2 = txt.readline().split()
+r1=int(f2[0])+r1
+r2=int(f2[1])+r2
+f3= txt.readline().split()
+r1=int(f3[0])+r1
+r2=int(f3[1])+r2
+f4 = txt.readline().split()
+r1=int(f4[0])+r1
+r2=int(f4[1])+r2
 txt.close()
 txt2=open("outinput.txt","w")
 if r1>r2:
---
+++ Игра/game.py
@@ -0,0 +1,3 @@
+txt=open("input.txt","r")
+f = txt.read()
+print(int(f)+5)
